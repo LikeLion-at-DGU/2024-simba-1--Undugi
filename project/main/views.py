bab7c18d194bfd13bffc7f8e48556ee17aa50ec5
seong-hyeon-Cho <whandrea@naver.com>
2024-06-06 15:37:50 +0900

    주석 달린 것  추가

--- project/main/views.py
+++ project/main/views.py
@@ -13,7 +13,17 @@ def main_page(request):
         calorie = request.POST.get('calorie')
         path_id = request.POST.get('path')
         
-        # 경로 및 기타 데이터를 저장하거나 처리하는 로직 추가
+        # 경로 및 기타 데이터를 저장하거나 처리하는 로직 추가한 것
+        start_building = Building.objects.get(id=start_building_id)
+        end_building = Building.objects.get(id=end_building_id)
+        path = Path.objects.get(id=path_id)
+        
+        Route.objects.create(
+            start_building=start_building,
+            end_building=end_building,
+            path=path,
+            calorie=calorie,
+        )
         return redirect('map_page')
 
     buildings = Building.objects.all()
@@ -21,5 +31,7 @@ def main_page(request):
     return render(request, 'main_page.html', {'buildings': buildings, 'paths': paths})
 
 def map_page(request):
-    # 지도로 이동하는 로직 추가
-    return render(request, 'map_page.html')
+    # 지도로 이동하는 로직 추가한 것
+    routes = Route.objects.all()
+
+    return render(request, 'map_page.html',{'routes': routes})
