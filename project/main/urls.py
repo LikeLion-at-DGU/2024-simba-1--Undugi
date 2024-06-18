5d2dfb553c166e0c8747076bc6f7f77bee347dd4
chaehyeo-n <parkche03@naver.com>
2024-06-18 21:23:55 +0900

    ✨ feat: 로그인 페이지 이동_0618

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from main import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
     path('map/', map_page, name='map_page'),
+    path('accounts/', include('accounts.urls')),
 ]
