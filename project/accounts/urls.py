3e2f27f3f7946c8d3a2d3c8f02ea233058a3bf57
chaehyeo-n <parkche03@naver.com>
2024-06-22 00:34:22 +0900

    🐛 fix: 회원가입 기능 수정_0622

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', login, name="login"),
     path('signup/', signup1, name="signup1"),
     path('signup2/', signup2, name="signup2"),
+    path('signup3/', signup3, name="signup3"),
     path('idpasswordfind/', idpasswordfind, name="idpasswordfind"),
     path('idfindv1/', idfindv1, name="idfindv1"),
     path('idfindv2/', idfindv2, name="idfindv2"),
