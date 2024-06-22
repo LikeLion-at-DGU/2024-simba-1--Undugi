5ca6dcc5a563da883e629a2930ae47e567ae4ae9
seong-hyeon-Cho <whandrea@naver.com>
2024-06-22 15:46:28 +0900

    ✨ ranking views.py html 연동

--- project/ranking/models.py
+++ project/ranking/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class Ranking(models.Model):
     ranking_id = models.AutoField(primary_key=True)
-    nickname = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100, unique=True)
     total_calories_burned = models.FloatField()
-    rank = models.IntegerField()
 
     def __str__(self):
-        return f"{self.nickname} - {self.rank}"
+        return f"{self.nickname} - {self.total_calories_burned}"
