47ab64565bf3e794184ebf2937d2e1459d9abae6
seong-hyeon-Cho <whandrea@naver.com>
2024-06-25 20:56:00 +0900

    feat.password change

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from accounts.models import Profile
+from django.contrib.auth.models import User
 
 class UserProfileForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), required=False)
+
     class Meta:
         model = Profile
-        fields = ['profileImage', 'nickName', 'goal', 'weight', 'major']
+        fields = ['profileImage', 'nickName', 'goal', 'weight', 'major', 'password']
         widgets = {
             'profileImage': forms.FileInput()
         }
@@ -13,3 +16,15 @@ class UserProfileForm(forms.ModelForm):
         super(UserProfileForm, self).__init__(*args, **kwargs)
         self.fields['nickName'].required = False
         self.fields['profileImage'].required = False
+
+    def save(self, commit=True):
+        profile = super(UserProfileForm, self).save(commit=False)
+        password = self.cleaned_data.get('password')
+        if password:
+            user = profile.user
+            user.set_password(password)
+            if commit:
+                user.save()
+        if commit:
+            profile.save()
+        return profile
